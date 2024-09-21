e21c44f9c04feaa30bd3fc258a16161e8df95bae
Shan1722 <shanskar8001@gmail.com>
2024-09-21 18:22:24 +0530

    refactor: move button to its own file.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-"use client";
-import { useState } from "react";
-
+import Button from "@/components/button";
 
 export default function Home() {
   return (
@@ -11,19 +9,5 @@ export default function Home() {
   );
 }
 
-function Button() {
-  const [count, setCount] = useState(0);
 
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  return (
-    <button
-      onClick={handleClick}
-      className="bg-blue-500 hover:bg-blue-700 rounded text-white font-bold px-4 py-2">
-      I have been clicked {count} times
-    </button>
-  ) 
-}
 
